8fde308837e10dd38cf0dda23341c96058463c5f
Jenish Lunagariya <jenishlunagariya71@gmail.com>
2024-09-24 22:16:13 +0530

    Nav route added

--- lib/views/register_view.dart
+++ lib/views/register_view.dart
@@ -1,7 +1,8 @@
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:firebase_core/firebase_core.dart';
+// import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:test_application/firebase_options.dart';
+import 'package:flutter/services.dart';
+// import 'package:test_application/firebase_options.dart';
 
 class RegisterView extends StatefulWidget {
   const RegisterView({super.key});
@@ -30,46 +31,55 @@ class _RegisterViewState extends State<RegisterView> {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        TextField(
-          controller: _email,
-          // enableSuggestions: false,
-          // autocorrect: false,
-          keyboardType: TextInputType.emailAddress,
-          decoration: const InputDecoration(hintText: "Email"),
-        ),
-        TextField(
-          controller: _password,
-          obscureText: true, // makes password invisible
-          enableSuggestions: false,
-          autocorrect: false,
-          decoration: const InputDecoration(hintText: "Password"),
-        ),
-        TextButton(
-          onPressed: () async {
-            final email = _email.text;
-            final password = _password.text;
-            try {
-              final userCredential = await FirebaseAuth.instance
-                  .createUserWithEmailAndPassword(
-                      email: email, password: password);
-              print(userCredential);
-            } on FirebaseAuthException catch (e) {
-              if (e.code == "weak-password") {
-                print("Weak password");
-              } else if (e.code == "email-already-in-use") {
-                print("Email already in use.");
-              } else if (e.code == "invalid-email") {
-                print("Invalid email entered.");
-              } else {
-                print(e);
+    return Scaffold(
+      appBar: AppBar(title: Text("Register View")),
+      body: Column(
+        children: [
+          TextField(
+            controller: _email,
+            // enableSuggestions: false,
+            // autocorrect: false,
+            keyboardType: TextInputType.emailAddress,
+            decoration: const InputDecoration(hintText: "Email"),
+          ),
+          TextField(
+            controller: _password,
+            obscureText: true, // makes password invisible
+            enableSuggestions: false,
+            autocorrect: false,
+            decoration: const InputDecoration(hintText: "Password"),
+          ),
+          TextButton(
+            onPressed: () async {
+              final email = _email.text;
+              final password = _password.text;
+              try {
+                final userCredential = await FirebaseAuth.instance
+                    .createUserWithEmailAndPassword(
+                        email: email, password: password);
+                print(userCredential);
+              } on FirebaseAuthException catch (e) {
+                if (e.code == "weak-password") {
+                  print("Weak password");
+                } else if (e.code == "email-already-in-use") {
+                  print("Email already in use.");
+                } else if (e.code == "invalid-email") {
+                  print("Invalid email entered.");
+                } else {
+                  print(e);
+                }
               }
-            }
-          },
-          child: const Text("Register"),
-        ),
-      ],
+            },
+            child: const Text("Register"),
+          ),
+          TextButton(
+              onPressed: () {
+                Navigator.of(context)
+                    .pushNamedAndRemoveUntil('/login/', (route) => false);
+              },
+              child: const Text("Already registered? Login here!"))
+        ],
+      ),
     );
   }
 }
