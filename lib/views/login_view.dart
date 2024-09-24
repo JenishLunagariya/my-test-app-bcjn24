bc9b43374bf2a97a4bcfe96dbd558326cdc9a94f
Jenish Lunagariya <jenishlunagariya71@gmail.com>
2024-09-24 22:15:53 +0530

    Nav routes added

--- lib/views/login_view.dart
+++ lib/views/login_view.dart
@@ -30,44 +30,52 @@ class _LoginViewState extends State<LoginView> {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        TextField(
-          controller: _email,
-          // enableSuggestions: false,
-          // autocorrect: false,
-          keyboardType: TextInputType.emailAddress,
-          decoration: const InputDecoration(hintText: "Email"),
-        ),
-        TextField(
-          controller: _password,
-          obscureText: true, // makes password invisible
-          enableSuggestions: false,
-          autocorrect: false,
-          decoration: const InputDecoration(hintText: "Password"),
-        ),
-        TextButton(
-          onPressed: () async {
-            final email = _email.text;
-            final password = _password.text;
-            try {
-              final userCredential = await FirebaseAuth.instance
-                  .signInWithEmailAndPassword(email: email, password: password);
-              print(userCredential);
-            } on FirebaseAuthException catch (e) {
-              if (e.code == "invalid-credential") {
-                print("Invalid Email or Password");
-              } else if (e.code == "wrong-password") {
-                print("Wrong Password");
-              } else {
-                print("Something Bad Happened");
-                print(e.code);
+    return Scaffold(
+      appBar: AppBar(title: const Text("Login View"),),
+      body: Column(
+        children: [
+          TextField(
+            controller: _email,
+            // enableSuggestions: false,
+            // autocorrect: false,
+            keyboardType: TextInputType.emailAddress,
+            decoration: const InputDecoration(hintText: "Email"),
+          ),
+          TextField(
+            controller: _password,
+            obscureText: true, // makes password invisible
+            enableSuggestions: false,
+            autocorrect: false,
+            decoration: const InputDecoration(hintText: "Password"),
+          ),
+          TextButton(
+            onPressed: () async {
+              final email = _email.text;
+              final password = _password.text;
+              try {
+                final userCredential = await FirebaseAuth.instance
+                    .signInWithEmailAndPassword(email: email, password: password);
+                print(userCredential);
+              } on FirebaseAuthException catch (e) {
+                if (e.code == "invalid-credential") {
+                  print("Invalid Email or Password");
+                } else if (e.code == "wrong-password") {
+                  print("Wrong Password");
+                } else {
+                  print("Something Bad Happened");
+                  print(e.code);
+                }
               }
-            }
-          },
-          child: const Text("Login"),
-        ),
-      ],
+            },
+            child: const Text("Login"),
+          ),
+          TextButton(
+              onPressed: () {
+                Navigator.of(context).pushNamedAndRemoveUntil('/register/', (route) => false);
+              },
+              child: const Text("Not Registered yet? Register here!"))
+        ],
+      ),
     );
   }
 }
