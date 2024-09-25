51adfa82dc6a6b7600b1b42f8c81952b10bc0a75
Jenish Lunagariya <jenishlunagariya71@gmail.com>
2024-09-25 09:48:03 +0530

    added main page

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:test_application/firebase_options.dart';
 import 'package:test_application/views/login_view.dart';
 import 'package:test_application/views/register_view.dart';
+import 'package:test_application/views/verify_email_view.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
@@ -26,26 +27,65 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return FutureBuilder(
-        future: Firebase.initializeApp(
-          options: DefaultFirebaseOptions.currentPlatform,
-        ),
-        builder: (context, snapshot) {
-          switch (snapshot.connectionState) {
-            case ConnectionState.done:
-              // final user = FirebaseAuth.instance.currentUser;
-              // if (user?.emailVerified ?? false) {
-              //   return const Text("Done...");
-              // } else {
-              //   // Navigator.of(context).push(MaterialPageRoute(
-              //   //     builder: (context) => const VerifyEmailView())); // push onto scaffold screen
-              //   return VerifyEmailView();
-              // }
+      future: Firebase.initializeApp(
+        options: DefaultFirebaseOptions.currentPlatform,
+      ),
+      builder: (context, snapshot) {
+        switch (snapshot.connectionState) {
+          case ConnectionState.done:
+            final user = FirebaseAuth.instance.currentUser;
+            // if (user?.emailVerified ?? false) {
+            //   return const Text("Done...");
+            // } else {
+            //   // Navigator.of(context).push(MaterialPageRoute(
+            //   //     builder: (context) => const VerifyEmailView())); // push onto scaffold screen
+            //   return VerifyEmailView();
+            // }
+            if (user != null) {
+              if (user.emailVerified) {
+                return NotesView();
+              } else {
+                return const VerifyEmailView();
+              }
+            } else {
               return const LoginView();
+            }
 
-            default:
-              return const CircularProgressIndicator(); //TODO: need to fix size
-          }
-        },
-      );
+          default:
+            return const CircularProgressIndicator(); //TODO: need to fix size
+        }
+      },
+    );
+  }
+}
+
+enum MenuAction { logout }
+
+class NotesView extends StatefulWidget {
+  const NotesView({super.key});
+
+  @override
+  State<NotesView> createState() => _NotesViewState();
+}
+
+class _NotesViewState extends State<NotesView> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Main UI"),
+        actions: [
+          PopupMenuButton<MenuAction>(
+              onSelected: (value) {},
+              itemBuilder: (context) {
+                return [
+                  const PopupMenuItem<MenuAction>(
+                      value: MenuAction.logout, child: Text("LogOut")),
+                ];
+              })
+        ],
+      ),
+      body: const Text("Main UI Text"),
+    );
   }
 }
